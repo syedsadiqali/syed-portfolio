8d11b174f602b3ef8d57748cd784a59c99b98647
Syed Sadiq Ali <syedsadiqali2018@gmail.com>
2022-11-18 13:13:30 +0530

    fixes

--- scripts/generate-rss.mjs
+++ scripts/generate-rss.mjs
@@ -3,6 +3,36 @@ import { join } from 'path';
 import RSS from 'rss';
 import matter from 'gray-matter';
 
+async function gql(query, variables = {}) {
+  const data = await fetch('https://api.hashnode.com/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      query,
+      variables
+    })
+  });
+
+  return data.json();
+}
+
+const GET_USER_ARTICLES = `
+    query GetUserArticles($page: Int!) {
+        user(username: "alisyed") {
+            publication {
+                posts(page: $page) {
+                    title
+                    brief
+                    slug
+					dateAdded
+                }
+            }
+        }
+    }
+`;
+
 async function generate() {
   const feed = new RSS({
     title: 'Syed Sadiq ali',
@@ -10,7 +40,12 @@ async function generate() {
     feed_url: 'https://syed.vercel.app/feed.xml'
   });
 
-  const posts = readdirSync(join(process.cwd(), 'data', 'blog'));
+  const posts = gql(GET_USER_ARTICLES, { page: 0 })
+    .then((result) => {
+      return { props: { posts: result.data.user.publication.posts } };
+    })
+    .catch((err) => console.log(err));
+
   posts.map((name) => {
     const content = readFileSync(join(process.cwd(), 'data', 'blog', name));
     const frontmatter = matter(content);
