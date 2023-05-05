11743aba57a2afaa3491ce8b84efe0a710d1afa2
Syed Sadiq ali <syedsadiqali2018@gmail.com>
2023-05-05 12:34:42 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -14,7 +14,7 @@ module.exports = {
 // https://securityheaders.com
 const ContentSecurityPolicy = `
 	default-src 'self';
-	script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com *.googlesyndication.com *.sonetel.com;
+	script-src 'self' 'unsafe-eval' 'unsafe-inline' *.youtube.com *.twitter.com cdn.usefathom.com *.googlesyndication.com *.sonetel.com *.vercel.live vercel.live;
 	child-src *.youtube.com *.google.com *.twitter.com;
 	style-src 'self' 'unsafe-inline' *.googleapis.com;
 	img-src * blob: data:;
